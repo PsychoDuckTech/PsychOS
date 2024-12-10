17da452b3cbee9466cb18d1922a24e8eba209c9f
GabiBrawl <gabiya219@gmail.com>
2024-12-10 19:48:35 +0000

    Add prototype0 board configuration and implement keyboard matrix handling

---
+++ source/include/boardConfiguration/prototype0.h
@@ -0,0 +1,34 @@
+/**
+ * Key Array Map for the Prototype 0 Matrix
+ */
+
+#include <Arduino.h>
+#include "usb_hid_keys.h" // by MightyPork on GitHub
+
+// Matrix configuration
+int const totalRows = 6;
+int const totalCols = 16;
+
+// Matrix Key Map
+const uint8_t keyMap[totalRows][totalCols] = {
+    {KEY_ESC, KEY_F1,KEY_F2,KEY_F3,KEY_F4, KEY_F5,KEY_F6,KEY_F7,KEY_F8, KEY_F9,KEY_F10,KEY_F11,KEY_F12, KEY_SYSRQ,KEY_INSERT, KEY_DELETE},
+    {KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8,KEY_9, KEY_0, KEY_APOSTROPHE, KEY_EQUAL, KEY_BACKSPACE, KEY_HOME, 0, 0},
+    {KEY_TAB, KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T, KEY_Y, KEY_U,KEY_I, KEY_O, KEY_P, KEY_LEFTBRACE, KEY_GRAVE, KEY_ENTER, KEY_END, 0},
+    {KEY_CAPSLOCK, KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J,KEY_K, KEY_L, KEY_SEMICOLON, KEY_HASHTILDE, KEY_BACKSLASH, 0, KEY_PAGEUP, 0},
+    {KEY_LEFTSHIFT, KEY_102ND, KEY_Z, KEY_X, KEY_C, KEY_V, KEY_B, KEY_N, KEY_M, KEY_COMMA, KEY_DOT, KEY_SLASH, 0, 0, 0, 0},
+    {KEY_LEFTCTRL, KEY_LEFTMETA, KEY_LEFTALT, KEY_SPACE, 0, 0, 0,KEY_RIGHTALT, 0, KEY_RIGHTCTRL, 0, 0, 0, 0, 0, 0}
+};
+
+// Matrix Key Name
+const char* keyName[totalRows][totalCols] = {
+    {"ESC", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "SYSRQ", "INSERT", "DELETE"},
+    {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "'", "=", "BACKSPACE", "HOME", "", ""},
+    {"TAB", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "`", "ENTER", "END", ""},
+    {"CAPSLOCK", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "#~", "\\", "", "PAGEUP", ""},
+    {"LSHIFT", "102ND", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "", "", "", ""},
+    {"LCTRL", "LMETA", "LALT", "SPACE", "", "", "", "RALT", "", "RCTRL", "", "", "", "", "", ""}
+};
+
+// GPIO assignment
+int const rowPins[totalRows] = {5, 6, 7, 18, 8, 3};
+int const colPins[totalCols] = {46, 9, 10, 11, 12, 13, 14, 20, 21, 35, 36, 37, 38, 39, 40, 41};
--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,75 +1,62 @@
 #include <Arduino.h>
-//#include <stdio.h>
+#include "boardConfiguration/prototype0.h" // The board specific configuration
 
-int count2 = 0;
-int count1 = 0;
+typedef struct {
+    uint8_t modifier;  // First byte for modifier keys
+    uint8_t reserved;  // Second byte (always 0)
+    uint8_t keycode[6];  // Up to 6 simultaneous key slots
+} USB_HID_Keyboard_Report_t;
 
-/*#define ROWS 6
-#define COLS 16
 
-const uint8_t key_map[ROWS][COLS] = {
-    {0x29, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40, 0x41, 0x42, 0x43, 0x44, 0x46, 0x49, 0x00, 0x00}, //row 1
-    {}, //row 2
-    {}, //row 3
-    {}, //row 4
-    {}, //row 5
-    {} //row 6
-};
-
+//TASKS ---------------------------------------------------------
 void KeystrokeHandler(void *parameters) {
-    for (;;) {
-        // Read key press
-        char key = getchar();
-        printf("Key pressed: %c\n", key);
-        vTaskDelay(10 / portTICK_PERIOD_MS);
-    }
-}*/
+    USB_HID_Keyboard_Report_t hid_report;
+    Serial.print("Started KeystrokeHandler Task\n");
 
-void Task2(void *parameters) {
     for (;;) {
-        Serial.print("Task2: ");
-        Serial.println(count2++);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        scanMatrix();
+        vTaskDelay(1 / portTICK_PERIOD_MS); // 1ms delay corresponding to a polling rate of 1000Hz
     }
 }
 
-void TaskSpam(void *parameters) {
-    for (;;) {
-        Serial.print("TaskSpam: ");
-        Serial.println(count1++);
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+void scanMatrix() {
+    for (int row; row < totalRows; row++) {
+        digitalWrite(rowPins[row], HIGH);
+        for (int col; col < totalCols; col++) {
+            if (digitalRead(colPins[col]) == HIGH) {
+                Serial.print("Key Pressed: " + String(keyMap[row][col]) + " Key name: " + keyName[row][col] + "\n");
+                Serial.print("Row: " + String(row) + " Col: " + String(col) + "\n");
+            }
+        }
+        digitalWrite(rowPins[row], LOW);
     }
 }
+
+void setupKeyboardMatrix(void *parameters) {
+    for (int i = 0; i < totalRows; i++) {pinMode(rowPins[i], OUTPUT);} // Set all row GPIO as OUTPUT
+    for (int i = 0; i < totalCols; i++) {pinMode(colPins[i], INPUT_PULLUP);} // Set all column GPIO as INPUT with PULLUP
+}
+// --------------------------------------------------------------
 
 void setup() {
     Serial.begin(115200);
 
-    /*xTaskCreate(
+    xTaskCreate(
+        setupKeyboardMatrix,
+        "Keyboard Matrix Setup",
+        1000,
+        NULL,
+        7,
+        NULL
+    );
+    xTaskCreate(
         KeystrokeHandler,
         "Keystroke Handler",
         1000,
         NULL,
-        2,
-        NULL
-    );*/
-    xTaskCreate(
-        Task2,
-        "T2",
-        1000,
-        NULL,
-        1,
-        NULL
-    );
-    xTaskCreate(
-        TaskSpam,
-        "TS",
-        1000,
-        NULL,
-        1,
+        6,
         NULL
     );
 }
 
-void loop() {
-    // Do nothing
-}
+// theres no loop function in FreeRTOS
