df465109efd6e0c8bb102726a1e159ccd72b8aaf
GabiBrawl <gabiya219@gmail.com>
2024-12-11 12:01:32 +0000

    rgfffgdgg

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,7 +1,5 @@
 #include <Arduino.h>
-//#include "translations/ptPT.h"
-#include "esp_task_wdt.h" // Include the watchdog timer header
-#include "boardConfig/test2x2.h" // The board specific configuration
+#include "boardConfiguration/test2x2.h" // The board specific configuration
 
 typedef struct {
     uint8_t modifier;  // First byte for modifier keys
@@ -21,8 +19,7 @@ void KeystrokeHandler(void *parameters) {
 
     for (;;) {
         scanMatrix();
-        esp_task_wdt_reset(); // Feed the watchdog timer
-        vTaskDelay(1000 / portTICK_PERIOD_MS); // 1ms delay corresponding to a polling rate of 1000Hz
+        vTaskDelay(10 / portTICK_PERIOD_MS); // 1ms delay corresponding to a polling rate of 1000Hz
     }
 }
 
@@ -31,7 +28,7 @@ void scanMatrix() {
         digitalWrite(rowPins[row], HIGH);
         for (int col = 0; col < totalCols; col++) {
             if (digitalRead(colPins[col]) == LOW) {
-                Serial.printf("Key Pressed: %s\n", keyName[row][col]);
+                Serial.printf("Key Pressed: %d Key name: %s\n", keyMap[row][col], keyName[row][col]);
                 Serial.printf("Row: %d Col: %d\n", row, col);
             }
         }
@@ -44,20 +41,12 @@ void setupKeyboardMatrix() {
     for (int i = 0; i < totalCols; i++) {pinMode(colPins[i], INPUT_PULLUP);} // Set all column GPIO as INPUT with PULLUP
     Serial.println("Setting up Keyboard Matrix done.");
 }
-
-// void submitKey();
 // --------------------------------------------------------------
 
 void setup() {
     delay(500);
     Serial.begin(115200);
     setupKeyboardMatrix();
-
-    // Initialize the task watchdog
-    esp_task_wdt_init(10, true); // Timeout of 10 seconds
-
-    // Add tasks to the watchdog
-    esp_task_wdt_add(NULL);
 
     xTaskCreate(
         KeystrokeHandler,
