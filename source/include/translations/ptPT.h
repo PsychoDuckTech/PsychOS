b6e24baa1e65af767185401a84abdaff82bba97f
GabiBrawl <gabiya219@gmail.com>
2024-12-28 13:10:48 +0000

    Add Portuguese translations for various tasks and module connection statuses

--- source/include/translations/ptPT.h
+++ source/include/translations/ptPT.h
@@ -2,7 +2,18 @@
 * Portuguese Portugal Translation
 */
 
-const char *OS_version = "PsychOS v0.0.0";
+const char *OS_version = "PsychOS v0.0.0 ";
+const char *byCompany = "por PsychoDuck Tech ";
 
+// keyScanning
+const char *task_keyScanning_started = "Tarefa de digitalização de teclas iniciada. ";
 
-const char *task_keystrokeHandler_started = "A Tarefa KeystrokeHandler começou.";
+// moduleConnectionHandler
+const char *task_moduleConnectionHandler_started = "Tarefa do Controlador de Conexão de Módulo Iniciada. ";
+const char *moduleConnected = "Módulo conectado! ";
+const char *moduleDisconnected = "Módulo desconectado! ";
+const char *waitingForConnection = "Aguardando conexão... ";
+const char *received = "Recebido: ";
+
+// knobHandler
+const char *task_knobHandler_started = "Tarefa do Controlador de Knob Rotativo Iniciada. ";
