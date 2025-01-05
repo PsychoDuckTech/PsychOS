d470b074ab0cc5ee2daf4271a80e542f23c74b95
GabiBrawl <gabiya219@gmail.com>
2025-01-05 20:11:24 +0000

    Update host communication bridge initialization; enable USB and ConsumerControl

--- source/include/tasks/hostCommunicationBridge.cpp
+++ source/include/tasks/hostCommunicationBridge.cpp
@@ -7,8 +7,7 @@ void hostCommunicationBridge(void* parameters) {
     hostMessageQueue = xQueueCreate(10, sizeof(HostMessage));
     HostMessage receivedMessage;
     
-    //USB.begin(); // Comment this line in case you need to use USB for debugging
-    //ConsumerControl.begin();
+    ConsumerControl.begin();
 
     Serial.println("Host Communication Bridge started.");
     
--- source/include/tasks/hostCommunicationBridge.h
+++ source/include/tasks/hostCommunicationBridge.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <Arduino.h>
-#include <USB.h>
 #include <USBHIDConsumerControl.h>
 
 enum MessageType {
